7a3c475a016b2d9689ab8a06399852ab4130e3bb
Jose Serrano <jose.serrano22@upr.edu>
2021-06-28 19:24:25 -0400

    Make tweetCell and the storyboard with constraint

---
+++ twitter/Views/TweetCell.m
@@ -0,0 +1,24 @@
+//
+//  TweetCell.m
+//  twitter
+//
+//  Created by jose1009 on 6/28/21.
+//  Copyright © 2021 Emerson Malca. All rights reserved.
+//
+
+#import "TweetCell.h"
+
+@implementation TweetCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
