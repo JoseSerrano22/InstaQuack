dbce984ecab8eac5fadce92a65dd54424560e721
Jose Serrano <jose.serrano22@upr.edu>
2021-06-28 17:06:02 -0400

    log out method

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "TimelineViewController.h"
+#import "LoginViewController.h"
 #import "APIManager.h"
+#import "AppDelegate.h"
 
 @interface TimelineViewController ()
 
@@ -36,6 +38,19 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)didTapLogout:(id)sender {
+    
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+
+    [[APIManager shared] logout];
+
+}
+
 
 /*
 #pragma mark - Navigation
